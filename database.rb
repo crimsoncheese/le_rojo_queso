1cb17555415ed29eb36b91663e3c19d58d8ffda0
Patrick (somewhat cool... eh maybe just ok) Nolan <pnolan@satx.rr.com>
2013-07-16 22:55:11 -0400

    deleted csvs

--- database.rb
+++ database.rb
@@ -1,51 +1,49 @@
-#This is my application
-require "mysql2"
-require "CSV"
-require "yaml"
-
+require 'mysql2'
+require 'yaml'
+require 'fileutils'
+require 'csv'
+load 'functions/functions.rb'
 config = YAML::load(open("#{File.expand_path(File.dirname(__FILE__))}/configs/configs.yml"))["mysql"]
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "patrick_class")
+client = Mysql2::Client.new(:host => config["host"], :username => config["username"], :database => config["database"])
 
 files = "*.csv"
 files = Dir.glob files
 p files
-
-
-client.query "truncate table customers"
-class FormatDate
-    def self.format(date)
-		Date.strptime(date, "%m/%d/%Y").strftime("%Y-%m-%d")
-	end
-end
+client.query("Truncate table customers")
 begin
-	# File.readlines(file).map {  |records|
-		# records = records.gsub("\n","")
-		# p records 
-		# records = records.split(",")
-	
-	# }
-	
-	files.each { |file|
-		p file
-		CSV.foreach(file, :headers => :first_row) { |records|
-			p records
-			fname = records[0].gsub("'","''")
-			lname = records[1].gsub("'","''")
-			dob = FormatDate.format(records[2])
-			dob = Date.strptime(records[2], "%m/%d/%Y").strftime("%Y-%m-%d")
-			age = records[3]
-			p "Insert into customers values('#{fname}', '#{lname}','#{dob}','#{age}')"
-				client.query "Insert into customers values('#{fname}', '#{lname}','#{age}','#{dob}','#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')"
-		
-		}
-	}
-	
-	
+  files.each { |file|
+  p file
+    CSV.foreach(file, :headers => :first_row) { |record|
+      p record
+      fname = record[0]
+      lname = record[1]
+      dob = FormatDate.new(record[2]).newMethod
+      #dob = Date.strptime(record[2], "%m/%d/%Y").strftime("%Y-%m-%d")
+      age = record[3]
+      #p "#{fname}, #{lname}, #{dob}, #{age}"
+      client.query "Insert into customers Values('#{fname}', '#{lname}', '#{age}', '#{dob}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')"
+    }
+    # File.readlines("customers.txt").map { |records|
+    # records = records.gsub("\n", "")
+    # records = records.split(",")
+    # fname = records[0]
+    # lname = records[1]
+    # dob = Date.strptime(records[2], "%m/%d/%Y").strftime("%Y-%m-%d")
+    # age = records[3]
+    # client.query "Insert into Customers Values('#{fname}', '#{lname}', '#{age}', '#{dob}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}')"
+    # }
+  }
 rescue Exception => e
-	@message = e.message
-	exit
+  $message = e.message
+  exit
 end
 
-files.each{|file| FileUtils.mv(file, "backup/")}
+# if $message == nil
+  files.each{|file| FileUtils.mv(file, "backups/")}
+# end
+# alert = []
+# alert << "Hello"
+
+# p alert
 
 
